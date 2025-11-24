9b77cd5df17c740dd2d5a7b0591e3d8db2e00515
coursework-2025 <oundovicent9@gmail.com>
2025-11-24 17:02:42 +0300

    oundo vicent
    connecting to databases

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const cors=require('cors')
 const express=require('express')
 const dotenv =require('dotenv');
+const connectDB=require('./src/database/database.js')
 dotenv.config()
 
 const PORT=process.env.PORT
@@ -8,6 +9,7 @@ const app=express()
 app.use(express.json)
 app.use(cors())
 
-app.listen(PORT,()=>{
+app.listen(PORT,async()=>{
     console.log(`server started at http://localhost:${PORT}`)
+    await connectDB()
 });
