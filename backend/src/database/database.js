8932720b84014590d6162db8602994392c7d1e7b
mubirusamuelalizeyuna-cloud <oundovicent9@gmail.com>
2025-11-24 16:53:16 +0300

    oundo
    creating database connection

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,1 +1,16 @@
-db 
+const dotenv = require('dotenv')
+const mongoose= require('mongoose')
+dotenv.config()
+
+exports.connectDB= async()=>{
+    try{
+        //connecting to the database
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("mongoose connected successfully")
+        
+
+    } catch(error){
+        console.error(error)
+        
+    }
+}
